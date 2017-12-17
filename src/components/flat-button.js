4216dc416d62957163065d607ebbea53d5bc12b5
Rodrigo Wippel <rodrigow1992@gmail.com>
2017-12-17 21:49:23 -0200

    componente flatbutton

--- src/components/flat-button.js
+++ src/components/flat-button.js
@@ -4,6 +4,30 @@ import styled from 'styled-components/native'
 import { blue, white } from 'style/colors'
 import { MaterialIcons  } from '@expo/vector-icons'
 
+// const style = StyleSheet.create({
+//   container: {
+//     position: absolute,
+//     bottom: 10,
+//     right: 10,
+//   },
+//   circle : {
+//     width: 70,
+//     height: 70,
+//     borderRadius: 70,
+//     backgroundColor: blue,
+//     alignItems: center,
+//     justifyContent: center
+//   }
+// })
+
+// const FlatButton = ({ title, onPress }) => (
+//   <TouchableHighlight style={style.container} onPress={onPress}>
+//     <View style={style.circle}>
+//       <MaterialIcons name='add' size={32} color={white} />
+//     </View>
+//   </TouchableHighlight>
+// )
+
 const TouchableHighlightStyled = styled.TouchableHighlight`
   position: absolute;
   bottom: 10px;
@@ -13,14 +37,16 @@ const TouchableHighlightStyled = styled.TouchableHighlight`
 const ViewStyled = styled.View`
   width: 70px;
   height: 70px;
-  borderRadius: 70px;
-  backgroundColor: ${blue};
+  border-radius: 70px;
+  background-color: ${blue};
   align-items: center;
   justify-content: center;
 `
 
+
+
 const FlatButton = ({ title, onPress }) => (
-  <TouchableHighlightStyled onPress={onPress}>
+  <TouchableHighlightStyled onPress={onPress} underlayColor={blue}>
     <ViewStyled>
       <MaterialIcons name='add' size={32} color={white} />
     </ViewStyled>
